9973f6f51817f1934e48a2657029f524fa0038f3
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2021-12-02 12:29:29 -0600

    return svg from display

--- scripts/utils.js
+++ scripts/utils.js
@@ -46,12 +46,17 @@ const displayCapsule = async(id) => {
     $("body").append(`<div id="displayed-capsule"><span id="close" onclick='closeDisplay()'>x</span>${svg}</div>`);
 };
 
-const displayCharacter = async(id) => {
+const displayCharacter = async(id, returnSVG=false) => {
     await closeDisplay();
     const uri = await characters.tokenURI(id);
     const decodedUri = JSON.parse(atob(uri.replace("data:application/json;base64,", "")))
     const image = atob(decodedUri.image.replace("data:image/svg+xml;base64,", ""));
-    $("body").append(`<div id="displayed-character"><span id="close" onclick='closeDisplay()'>x</span>${image}</div>`);
+    if (returnSVG) {
+        return image;
+    }
+    else {
+        $("body").append(`<div id="displayed-character"><span id="close" onclick='closeDisplay()'>x</span>${image}</div>`);
+    }
 };
 
 const populateMyCharacters = async() => {
@@ -73,14 +78,37 @@ const closeDisplay = async() => {
     $(`#displayed-character`).remove();
 };
 
+
+const equipmentMap = new Map([[0, "Weapon"], [1, "Chest"], [2, "Head"], [3, "Legs"], 
+                            [4, "Vehicle"], [5, "Arms"], [6, "Artifact"], [7, "Ring"]]);
+
 const updateEquipmentUpgrade = async() => {
     const id = $("#upgrade-char").val();
     const equipmentType = $("#upgrade-eq-type").val();
     $("#upgrade-eq-amount").empty(); 
     $("#upgrade-eq-amount").append(`<option value="">-</option>`);
+    const equipmentLevels = await characterStorage.equipments(id);
+
+    if (id) {
+        $("#upgrade-equip-img").empty();
+        $("#upgrade-equip-img").append(await isolateIMG(id, 'upgrade-equip-svg'));
+        let charIMG = document.getElementById("upgrade-equip-svg");
+        charIMG.setAttribute('viewBox', '0 0 ' + 1200 + ' ' + 900);
+
+        $("#equip-stats-1").empty();
+        $("#equip-stats-2").empty();
+        for (let i = 0; i < 8; i++) {
+            if (i < 4) {
+                $("#equip-stats-1").append(`<span>${equipmentMap.get(i)}: Level ${equipmentLevels[i]}</span>`);
+            }
+            else {
+                $("#equip-stats-2").append(`<span>${equipmentMap.get(i)}: Level ${equipmentLevels[i]}</span>`);
+            }
+        }
+        
+    }
 
     if (id && equipmentType) {
-        const equipmentLevels = await characterStorage.equipments(id);
         const currentLevel = equipmentLevels[equipmentType-1];
         const levelsLeft = 4 - currentLevel;
         for (let i = 1; i <= levelsLeft; i++) {
@@ -95,14 +123,53 @@ const updateLevelUpPoints = async() => {
     $("#level-up-amount").append(`<option value="">-</option>`);
 
     if (id) {
+        $("#level-up-char-img").empty();
+        $("#level-up-char-img").append(await isolateIMG(id, 'level-up-svg'));
+        let charIMG = document.getElementById("level-up-svg");
+        charIMG.setAttribute('viewBox', '0 0 ' + 1200 + ' ' + 900);
+
         const stats = await characterStorage.characters(id);
         const currentBasePoints = stats.basePoints_;
+        $("#level-up-char-stats").empty();
+        $("#level-up-char-stats").append(`<h3>Current Base Points: ${currentBasePoints}</h3>`);
+
         const pointsLeft = 50 - currentBasePoints;
         for (let i = 1; i <= pointsLeft; i++) {
             $("#level-up-amount").append(`<option value="${i}">${i}</option>`); 
         }
     }
 };
+
+const isolateIMG = async(id, elemID) => {
+    const svg = await displayCharacter(id, true);
+
+    // Get opening svg tag
+    const svgOpenStart = svg.indexOf('<svg');
+    const svgOpenEnd = svg.indexOf('>') + 1;
+    const svgOpen = svg.substring(svgOpenStart, svgOpenStart + 4) + ` id='${elemID}' class="zoom"` +  svg.substring(svgOpenStart + 4, svgOpenEnd);
+
+    // Get style tag
+    const styleStart = svg.indexOf('<style');
+    const styleEnd = svg.indexOf('/style>') + 7;
+    const style = svg.substring(styleStart, styleEnd);
+
+    // Get character image
+    const imgStart = svg.indexOf('<g transform');
+    const nextG = svg.indexOf('</g>', imgStart);
+    const imgEnd = svg.indexOf('</g>', nextG + 1) + 4;
+    const img = svg.substring(imgStart, imgEnd);
+
+    // Get header text
+    const headerStart = svg.indexOf('<text');
+    const headerEnd = svg.indexOf('/text') + 5;
+    const header = svg.substring(headerStart, headerEnd);
+    console.log(header)
+
+    // Assemble
+    const imgToDisplay = svgOpen + style + img + header + "</svg>";
+
+    return imgToDisplay
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
