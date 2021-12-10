210bac90eb91db115e32b395f01b68b42b8b23bb
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2021-12-09 22:01:41 -0600

    add confirm button and id label

--- scripts/utils.js
+++ scripts/utils.js
@@ -453,10 +453,10 @@ const displaySelect = async(asset, action) => {
             else if (currentlySelected.has(id)) {
                 active = "active";
             }
-            assetsJSX += `<div class='${asset}-div ${active}' id="${asset}-${id}" onclick="selectForAction(${id}, '${asset}', '${action}')">${svg}</div>`
+            assetsJSX += `<div class='${asset}-div ${active}' id="${asset}-${id}" onclick="selectForAction(${id}, '${asset}', '${action}')">${svg}<h2>${id}</h2></div>`
         }
     
-        let fakeJSX = `<div id="${asset}-select"><div id="close-div"><span id="close" onclick='closeDisplay()'>x</span></div>${assetsJSX}<div class="selected-${asset}s-${action}" id="selected-${asset}s-big">Selected: None</div></div>`;
+        let fakeJSX = `<div id="${asset}-select"><div id="close-div"><span id="close" onclick='closeDisplay()'>x</span></div>${assetsJSX}<div id='bottom-div'><div class="selected-${asset}s-${action}" id="selected-${asset}s-big">Selected: None</div><button id='confirm' onclick="closeDisplay()">CONFIRM</button></div></div>`;
         let height = $(document).height();
         $("body").append(`<div id='block-screen' style="height:${height}px" onclick='closeDisplay()'></div>`);
         $("body").append(fakeJSX);
